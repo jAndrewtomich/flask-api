20a1aed009035dbf75c05d3ea4f7208c3bc96928
jatomic <johnatomich@gmail.com>
2021-12-14 15:39:04 -0600

    Connect frontend to backend

--- app/frontend/src/App.js
+++ app/frontend/src/App.js
@@ -5,13 +5,26 @@ import axios from 'axios'
 
 function App() {
   const [getMessage, setGetMessage] = useState({})
+
+  useEffect(()=>{
+    axios.get("http://localhost:5000/flask/endpoint").then(response => {
+      console.log("SUCCESS", response)
+      setGetMessage(response)
+    }).catch(error => {
+      console.log(error)
+    })
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          React + Flask Tutorial
         </p>
+        <div>{getMessage.status === 200 ?
+          <h3>{getMessage.data.text}</h3>
+          :
+          <h3>LOADING</h3>}</div>
         <a
           className="App-link"
           href="https://reactjs.org"
