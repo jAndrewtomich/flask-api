77cd14610e225a5e602b47fbb2e613187b93c4fa
jatomic <johnatomich@gmail.com>
2021-12-14 15:17:54 -0600

    Offer default API response for testing

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,23 @@
-from flask import Flask, request, current_app, send_from_directory
-from flask_restful import Api, Resource, reqparse
+from flask import Flask
+from flask_restful import Api
 from flask_cors import CORS
-from main.api_handler import ApiHandler
 from config import Config
+from app.main.api_handler import ApiHandler
 
 api = Api()
+cors = CORS()
 
 
 def create_app(config_class=Config):
     app = Flask(__name__, static_url_path="", static_folder="frontend/build")
     app.config.from_object(config_class)
 
+    cors.init_app(app)
     api.init_app(app)
 
     from app.main import bp as main_bp 
     app.register_blueprint(main_bp)
+
+    return app
+
+api.add_resource(ApiHandler, "/flask/endpoint")
