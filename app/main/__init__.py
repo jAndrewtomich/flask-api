10566beca56c1c13030d0a9afb47e3a8b3dc90f2
jatomic <johnatomich@gmail.com>
2021-12-17 17:24:00 -0600

    Block: blueprint <flask blueprint>, Change: Move NewsHandler and ApiHandler here -> Can be initialized along with app.

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -2,4 +2,7 @@ from flask import Blueprint
 
 bp = Blueprint("main", __name__)
 
-from app.main import routes
+from app.main import routes, api_handler, news_handler
+
+api_h = api_handler.ApiHandler()
+news_h = news_handler.NewsHandler()
