6b6e08a9309066c352b9b07c77e2a43307bcbe78
jatomic <johnatomich@gmail.com>
2021-12-17 09:46:12 -0600

    Block: create_app(), Change: move config to env variable or 'development' by default -> environment is now selectable and read from config.py

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_restful import Api
 from flask_cors import CORS
 from config import Config
+import os
 from app.main.api_handler import ApiHandler
 
 api = Api()
@@ -10,7 +11,8 @@ cors = CORS()
 
 def create_app(config_class=Config):
     app = Flask(__name__, static_url_path="", static_folder="frontend/build")
-    app.config.from_object(config_class)
+    env_config = os.getenv("APP_SETTINGS", "config.DevelopmentConfig")
+    app.config.from_object(env_config)
 
     cors.init_app(app)
     api.init_app(app)
