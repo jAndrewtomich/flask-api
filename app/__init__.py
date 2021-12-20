1cbaba4d18e8f61fc0a491595cb653d1ac27df23
jatomic <johnatomich@gmail.com>
2021-12-20 00:21:16 -0600

    Add logging

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,8 @@ from flask import Flask
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
-import os
+import os, logging
+from logging.handlers import RotatingFileHandler
 
 db = SQLAlchemy()
 cors = CORS()
@@ -22,5 +23,17 @@ def create_app():
 
     from app.main import bp as main_bp
     app.register_blueprint(main_bp)
+
+    if not app.debug:
+        if not os.path.exists('logs'):
+            os.mkdir('logs')
+        file_handler = RotatingFileHandler('logs/flask_api.log', maxBytes=10240, backupCount=10)
+        file_handler.setFormatter(logging.Formatter(
+            '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d'))
+        file_handler.setLevel(logging.INFO)
+        app.logger.addHandler(file_handler)
+
+        app.logger.setLevel(logging.INFO)
+        app.logger.info('Flask API Startup')
 
     return app
