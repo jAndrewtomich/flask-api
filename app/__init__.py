72bcc4a8722240ee81610d69de6430ba31f34669
jatomic <johnatomich@gmail.com>
2021-12-19 23:19:24 -0600

    Clean up format

--- app/__init__.py
+++ app/__init__.py
@@ -1,29 +1,26 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Api
+from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
-from config import DevelopmentConfig
+import os
 
 db = SQLAlchemy()
-api = Api()
 cors = CORS()
+api = Api()
 
-
-def create_app(config_class=DevelopmentConfig):
+def create_app():
     app = Flask(__name__, static_url_path="", static_folder="frontend/build")
-    app.config.from_object(config_class)
+    env_config = os.environ.get("APP_SETTINGS", "config.DevelopmentConfig")
+    app.config.from_object(env_config)
 
     db.init_app(app)
     with app.app_context():
         db.create_all()
+
     cors.init_app(app)
     api.init_app(app)
 
-    from app.main import bp as main_bp 
+    from app.main import bp as main_bp
     app.register_blueprint(main_bp)
 
-    from app.main import news_h, api_h
-
     return app
-
-from app import models 
