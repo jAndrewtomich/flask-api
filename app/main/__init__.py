cd2924b2b3ea6a69fb8b681823f433cc77d4928d
jatomic <johnatomich@gmail.com>
2021-12-19 23:18:45 -0600

    Change import of news and api_handler

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -2,7 +2,4 @@ from flask import Blueprint
 
 bp = Blueprint("main", __name__)
 
-from app.main import routes, api_handler, news_handler
-
-api_h = api_handler.ApiHandler()
-news_h = news_handler.NewsHandler()
+from app.main import routes
