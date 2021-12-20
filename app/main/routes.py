457fbb4e420eb5b4f7c6f982f38ccf48a7aa8d6b
jatomic <johnatomich@gmail.com>
2021-12-19 23:23:45 -0600

    Add function before_first_app_request to populate database.

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,8 +1,16 @@
-from flask import current_app
-from flask.helpers import send_from_directory
-from app.main import bp
+from app.main import bp, api_handler, news
+from app import api, db
+from app import models
+
+@bp.before_app_first_request
+def get_news():
+    hlList = news.extract_headlines('https://news.ycombinator.com')
+    api.add_resource(api_handler.ApiHandler, '/')
+    news.generate_summaries(hlList)
 
 
-@bp.route('/', defaults={"path":""})
-def serve(path):
-    return send_from_directory(current_app.static_folder, "index.html")
+@bp.route('/', defaults={"path": ""})
+def server(path):
+    articles = db.session.query(models.Article).all()
+    return {"result": "Success" if articles is not None else "Failure",
+    "data": [{'title': a.title, 'summary': a.summary, 'keywords': a.keywords, 'link': a.link} for a in articles]}
