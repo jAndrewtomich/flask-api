fb2d15d2fb18f476a0e69a06096519a730500499
jatomic <johnatomich@gmail.com>
2021-12-20 21:56:45 -0600

    Trying to fix it

--- app/main/news_handler.py
+++ app/main/news_handler.py
@@ -1,23 +1,17 @@
 from requests import get # http 'get' requests
-from requests.exceptions import ConnectionError as RqConnError
+from requests.exceptions import ConnectionError, MissingSchema, ReadTimeout
 from bs4 import BeautifulSoup # html parsing
 from gensim.summarization.summarizer import summarize # extractive text summarization
-from gensim.summarization import keywords # get keywords from summary
+from gensim.summarization import keywords
+from requests.models import MissingSchema # get keywords from summary
 from app.models import Article
 from app import db
+import random
 
 
-class NewsHandler():
-    def __init__(self, file=None):
-        if file:
-            with open(file, 'r') as reader:
-                urls = reader.readlines()
-
-        self.urls = urls if file else input('Enter URL: ')
-        self.hlList = self.extract_headlines()
-
-    def extract_headlines(url):
-        hlList = []
+def extract_headlines(urlList):
+    hlList = []
+    for url in urlList:
         print("Extracting Stories ...")
         response = get(url)
         content = response.content
@@ -26,39 +20,39 @@ class NewsHandler():
         for tag in soup.find_all("td", attrs={"class": "title", "valign": ""}):
             if (link_url := tag.a["href"])[:4] != "http":
                 link_url = "https://news.ycombinator.com/" + link_url
-            
-            hlList.append(link_url)
-        
-        return hlList[:-1]
 
-    def generate_summaries(self):
+            hlList.append([link_url, tag.a.text])
+    
+    return hlList[:-1]
 
-        def get_only_text(url):
-            try:
-                page = get(url)
-                soup = BeautifulSoup(page.content, "lxml")
-                text = ' '.join(map(lambda p: p.text.strip(), soup.find_all('p')))
-                title = "Default Title" if not soup.title else ' '.join(soup.title.stripped_strings)
+def generate_summaries(hlList):
 
-            except RqConnError as e:
-                title, text = None, e
+    def get_only_text(url):
+        try:
+            page = get(url, timeout=10)
+            soup = BeautifulSoup(page.content, "lxml")
+            text = ' '.join(map(lambda p: p.text.strip(), soup.find_all('p')))
+            title = "Default Title" if not soup.title else ' '.join(soup.title.stripped_strings)
 
-            return title, text
+        except (ConnectionError, MissingSchema, ReadTimeout) as e:
+            title, text = None, e
 
-        for hl in self.hlList:
-            title, text = get_only_text(hl)
-        
-            if title is None:
-                print(f"Error : {hl} : {text}")
-                continue
+        return title, text
 
-            k_l_words = keywords(text, ratio=0.1, lemmatize=True)
+    for hl in hlList:
+        title, text = get_only_text(hl[0])
+    
+        if title is None:
+            print(f"Error : {hl[0]} : {text}")
+            continue
 
-            try:
-                summary = summarize(repr(text), ratio=0.1)
-            except ValueError as e:
-                summary = "Inadequate text structure.  This text cannot be summarized.  This is default text.  This might be summarized."
+        k_l_words = keywords(text, lemmatize=True)
 
-            article = Article(title=title, summary=summary, keywords=k_l_words, link=hl)
-            db.session.add(article)
-            db.session.commit()
+        try:
+            summary = summarize(repr(text), ratio=0.1)
+            summary = ' '.join(c for c in summary.split() if c.isalnum())
+        except ValueError:
+            summary = "Inadequate text structure.  This text cannot be summarized.  This is default text.  This might be summarized."
+        print(k_l_words)
+        db.session.add(Article(title=hl[1], summary=' '.join(summary.split()[:100]), keywords=' '.join(random.sample(k_l_words.split(), int(len(k_l_words.split()) * .20))) if len(k_l_words.split()) >= 5 else k_l_words, link=hl[0]))
+        db.session.commit()
--- app/main/routes.py
+++ app/main/routes.py
@@ -1,12 +1,12 @@
-from app.main import bp, api_handler, news
+from app.main import bp, api_handler, news_handler
 from app import api, db
 from app import models
 
 @bp.before_app_first_request
 def get_news():
-    hlList = news.extract_headlines('https://news.ycombinator.com')
+    hlList = news_handler.extract_headlines(['https://news.ycombinator.com', 'https://news.ycombinator.com/news?p=2', 'https://news.ycombinator.com/news?p=3', 'https://news.ycombinator.com/news?p=4', 'https://news.ycombinator.com/news?p=5'])
     api.add_resource(api_handler.ApiHandler, '/')
-    news.generate_summaries(hlList)
+    news_handler.generate_summaries(hlList)
 
 
 @bp.route('/', defaults={"path": ""})
