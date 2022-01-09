d894a32ab95c52e99df4b405c6e326b02cd9f6e5
jatomic <johnatomich@gmail.com>
2022-01-08 21:43:13 -0600

    Formatting change

--- app/main/routes.py
+++ app/main/routes.py
@@ -2,12 +2,14 @@ from app.main import bp, api_handler, news_handler
 from app import api, db
 from app import models
 
+
 @bp.before_app_first_request
 def first_get_news():
     api.add_resource(api_handler.ApiHandler, '/')
 #    hlList = news_handler.extract_headlines(['https://news.ycombinator.com', 'https://news.ycombinator.com/news?p=2', 'https://news.ycombinator.com/news?p=3', 'https://news.ycombinator.com/news?p=4', 'https://news.ycombinator.com/news?p=5'])
 #    news_handler.generate_summaries(hlList)
 #    api.add_resource(api_handler.ApiHandler, '/')
+
 
 @bp.before_app_request
 def get_news():
@@ -16,7 +18,8 @@ def get_news():
     if test_link is not None: test_link = test_link.link
     if test_link != hlList[0][0]:
         news_handler.generate_summaries(hlList)
-    
+
+
 @bp.route('/', defaults={"path": ""})
 def server(path):
     articles = db.session.query(models.Article).all()
